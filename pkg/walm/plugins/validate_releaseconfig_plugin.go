8cb39f88e28b58672a17cd1b1d91637cdcba42b0
yang.cong <yang.cong@transwarp.io>
2019-02-21 15:24:48 +0800

    #WARP-29627 add label pod plugin

--- pkg/walm/plugins/validate_releaseconfig_plugin.go
+++ pkg/walm/plugins/validate_releaseconfig_plugin.go
@@ -1,22 +1,15 @@
 package plugins
 
 import (
-	"k8s.io/helm/pkg/hapi/release"
 	"k8s.io/helm/pkg/walm"
-	"bytes"
-	"k8s.io/helm/pkg/tiller/environment"
-	"k8s.io/helm/pkg/kube"
-	"k8s.io/cli-runtime/pkg/genericclioptions/resource"
 	"fmt"
 	"transwarp/release-config/pkg/apis/transwarp/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"strings"
-	"github.com/sirupsen/logrus"
-	"github.com/ghodss/yaml"
 )
 
 const (
 	AutoGenLabelKey = "auto-gen"
+	ValidateReleaseConfigPluginName = "ValidateReleaseConfig"
 )
 
 // ValidateReleaseConfig plugin is used to make sure:
@@ -24,27 +17,22 @@ const (
 // 2. ReleaseConfig has the same namespace and name with the release
 
 func init() {
-	walm.Register("ValidateReleaseConfig", &walm.WalmPluginRunner{
+	walm.Register(ValidateReleaseConfigPluginName, &walm.WalmPluginRunner{
 		Run:  ValidateReleaseConfig,
 		Type: walm.Pre_Install,
 	})
 }
 
 func ValidateReleaseConfig(context *walm.WalmPluginManagerContext, args string) (err error) {
-	resources, err := buildResources(context.KubeClient, context.R)
-	if err != nil {
-		return err
-	}
-	releaseConfigResources := []*resource.Info{}
-	newResources := []runtime.Object{}
-	for _, resource := range resources {
-		if resource.Object.GetObjectKind().GroupVersionKind().Kind == "ReleaseConfig" {
-			if resource.Name != context.R.Name {
+	releaseConfigKey := walm.BuildResourceKey("ReleaseConfig", context.R.Namespace, context.R.Name)
+	releaseConfigResources := []runtime.Object{}
+	for resourceKey, resource := range context.Resources {
+		if resource.GetObjectKind().GroupVersionKind().Kind == "ReleaseConfig" {
+			delete(context.Resources, resourceKey)
+			if resourceKey != releaseConfigKey {
 				continue
 			}
 			releaseConfigResources = append(releaseConfigResources, resource)
-		} else {
-			newResources = append(newResources, resource.Object)
 		}
 	}
 
@@ -52,11 +40,11 @@ func ValidateReleaseConfig(context *walm.WalmPluginManagerContext, args string) 
 	if releaseConfigNum == 0 {
 		return fmt.Errorf("release must have one ReleaseConfig resource")
 	} else if releaseConfigNum == 1 {
-		return nil
+		context.Resources[releaseConfigKey] = releaseConfigResources[0]
 	} else if releaseConfigNum == 2 {
 		var autoGenReleaseConfig, releaseConfig *v1beta1.ReleaseConfig
 		for _, releaseConfigResource := range releaseConfigResources {
-			rc := releaseConfigResource.Object.(*v1beta1.ReleaseConfig)
+			rc := releaseConfigResource.(*v1beta1.ReleaseConfig)
 			if len(rc.Labels) > 0 && rc.Labels[AutoGenLabelKey] == "true" {
 				autoGenReleaseConfig = rc
 			} else {
@@ -82,11 +70,7 @@ func ValidateReleaseConfig(context *walm.WalmPluginManagerContext, args string) 
 			releaseConfig.Labels[k] = v
 		}
 
-		newResources = append(newResources, releaseConfig)
-		context.R.Manifest, err = buildManifest(newResources)
-		if err != nil {
-			return err
-		}
+		context.Resources[releaseConfigKey] = releaseConfig
 	} else if releaseConfigNum > 2 {
 		return fmt.Errorf("release can not have more than one ReleaseConfig resource")
 	}
@@ -94,20 +78,4 @@ func ValidateReleaseConfig(context *walm.WalmPluginManagerContext, args string) 
 	return
 }
 
-func buildResources(kubeClient environment.KubeClient, r *release.Release) (kube.Result, error) {
-	return kubeClient.BuildUnstructured(r.Namespace, bytes.NewBufferString(r.Manifest))
-}
 
-func buildManifest(resources []runtime.Object) (string,  error) {
-	var sb strings.Builder
-	for _, resource := range resources {
-		resourceBytes, err := yaml.Marshal(resource)
-		if err != nil {
-			logrus.Errorf("failed to marshal k8s resource : %s", err.Error())
-			return "", err
-		}
-		sb.WriteString("\n---\n")
-		sb.Write(resourceBytes)
-	}
-	return sb.String(), nil
-}
